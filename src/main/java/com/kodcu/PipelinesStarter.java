1875582c03050fa289a9d33e8e665df36debac90
avm-blog <49306255+avm-blog@users.noreply.github.com>
2019-04-20 15:40:40 +0530

    Add files via upload

---
+++ src/main/java/com/kodcu/PipelinesStarter.java
@@ -0,0 +1,20 @@
+package com.kodcu;
+
+/*
+ * Created by XYZ on 17/06/2018
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class PipelinesStarter {
+
+    public static void main(String[] args){
+        SpringApplication.run(PipelinesStarter.class, args);
+    }
+}
---
+++ src/main/java/com/kodcu/controller/WelcomePageController.java
@@ -0,0 +1,18 @@
+package com.kodcu.controller;
+
+/*
+ * Created by XYZ on 17/06/2018
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class WelcomePageController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getPage(){
+        return "index";
+    }
+}
